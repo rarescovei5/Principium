9978aa80650fc9b0d11a10922ea8be51c69966b1
Covei Rares <rarescovei5@gmail.com>
2025-05-01 15:37:39 +0300

    Allow requests from VSCODE

--- backend/src/config/corsOptions.ts
+++ backend/src/config/corsOptions.ts
@@ -1,11 +1,24 @@
-const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_PATH];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'vscode-webview-resource://*',
+  'vscode-webview://*',
+  process.env.FRONTEND_PATH,
+];
 const corsOptions = {
   origin: (origin: any, callback: any) => {
+    console.log(origin);
     if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+      return callback(null, true);
     }
+
+    if (
+      origin.startsWith('vscode-webview-resource://') ||
+      origin.startsWith('vscode-webview://')
+    ) {
+      return callback(null, true);
+    }
+
+    callback(new Error('Not allowed by CORS'));
   },
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credentials: true,
